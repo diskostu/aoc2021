4178ab12408f5e3afcf13527fecad6fe216793d0
diskostu <denny.kluge@gmail.com>
2021-12-02 20:48:24 +0100

    finished day01 / task2

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -3,15 +3,15 @@ package day01
 import util.Utils
 
 fun main() {
-    task1()
+    val input = Utils().readFileAsIntList("day01/input.txt")
+    task1(input) // 1766
+    task2(input) // 1797
 }
 
-private fun task1() {
-    val depths = Utils().readFileAsIntList("day01/input.txt")
-
+private fun task1(input: List<Int>) {
     var depthsGreater = 0
     var previousDepth = -1
-    for (depth in depths) {
+    for (depth in input) {
         if (previousDepth == -1) {
             previousDepth = depth
             continue
@@ -24,5 +24,22 @@ private fun task1() {
         previousDepth = depth
     }
 
-    println("task1: $depthsGreater")
+    println("task1 = $depthsGreater")
+}
+
+private fun task2(input: List<Int>) {
+    var greaterDepths = 0
+    for (i in 0..input.lastIndex - 3) {
+        val triple1 = Triple(input[i], input[i + 1], input[i + 2])
+        val triple2 = Triple(input[i + 1], input[i + 2], input[i + 3])
+
+        if (tripleSum(triple2) > tripleSum(triple1)) greaterDepths++
+    }
+
+    println("task2 = $greaterDepths")
+
+}
+
+private fun tripleSum(triple: Triple<Int, Int, Int>): Int {
+    return triple.first + triple.second + triple.third
 }
