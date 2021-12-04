22a520ff08d51f7560290ad29359303ae6696731
diskostu <denny.kluge@gmail.com>
2021-12-04 20:14:16 +0100

    finished day02 / task2

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -3,7 +3,7 @@ import util.Utils
 fun main() {
     val input = Utils().readFileAsPairStringIntList("input_day02.txt")
     task1(input) // 1660158
-    //task2(input) // ???
+    task2(input) // 1604592846
 }
 
 fun task1(input: List<Pair<String, Int>>) {
@@ -20,7 +20,20 @@ fun task1(input: List<Pair<String, Int>>) {
     println("task1 = ${posX * depth}")
 }
 
-private fun task2(input: List<Int>) {
+private fun task2(input: List<Pair<String, Int>>) {
+    var posX = 0
+    var depth = 0
+    var aim = 0
+    for (pair in input) {
+        when (pair.first) {
+            "forward" -> {
+                posX += pair.second
+                depth += aim*pair.second
+            }
+            "up" -> aim -= pair.second
+            "down" -> aim += pair.second
+        }
+    }
 
-
+    println("task2 = ${posX * depth}")
 }
