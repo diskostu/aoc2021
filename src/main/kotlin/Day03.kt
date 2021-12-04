4209611209b2687ecc29d30db1919ea473580b82
diskostu <denny.kluge@gmail.com>
2021-12-04 21:28:01 +0100

    finished day03 / task2

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -3,7 +3,7 @@ import util.Utils
 fun main() {
     val input = Utils().readFileAsStringList("input_day03.txt")
     task1(input) // 749376
-    task2(input) // ???
+    task2(input) // 2372923
 }
 
 fun task1(input: List<String>) {
@@ -31,5 +31,38 @@ fun task1(input: List<String>) {
 }
 
 fun task2(input: List<String>) {
-    TODO("Not yet implemented")
+    val oxygenRating = reduceList(input, true)
+    val co2Rating = reduceList(input, false)
+
+    println("task2 = ${oxygenRating.toInt(2) * co2Rating.toInt(2)}")
+}
+
+private fun reduceList(list: List<String>, biggerIsBetter: Boolean): String {
+    val rowLength = list[0].length
+    var mutableList = list.toMutableList()
+
+    for (i in 0 until rowLength) {
+        var count0 = 0
+        var count1 = 0
+        for (s in mutableList) {
+            if (s[i] == '0') count0++ else count1++
+
+        }
+
+        var charToFilter: Char
+        if (mutableList.size > 1) {
+            charToFilter = if (count0 > count1) {
+                if (biggerIsBetter) '0' else '1'
+            } else {
+                if (biggerIsBetter) '1' else '0'
+            }
+
+            mutableList = mutableList.stream().filter { s -> s[i] == charToFilter }.toList()
+        } else {
+            return mutableList[0]
+        }
+    }
+
+    if (mutableList.size == 1) return mutableList[0]
+    else throw IllegalStateException("more than 1 element in list")
 }
