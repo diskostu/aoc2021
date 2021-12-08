25678226ff02b849e90d676658c4ee776cff0dbc
diskostu <denny.kluge@gmail.com>
2021-12-08 19:54:35 +0100

    finished day08 / task1

---
+++ src/main/kotlin/day08/Day08.kt
@@ -0,0 +1,53 @@
+package day08
+
+import util.Utils
+
+fun main() {
+    val currentDay = Day08()
+    val input = Utils().readFileAsStringList("input_day08.txt")
+
+    println("task1: ${currentDay.calcTask1(input)}") // 355
+}
+
+
+class Day08 {
+
+    fun calcTask1(input: List<String>): Int {
+        // 0 - 6
+        // 1 - 2
+        // 2 - 5
+        // 3 - 5
+        // 4 - 4
+        // 5 - 5
+        // 6 - 6
+        // 7 - 3
+        // 8 - 7
+        // 9 - 6
+
+        // unique:
+        // 1 - 2
+        // 4 - 4
+        // 7 - 3
+        // 8 - 7
+
+        var sumForTask = 0
+        for (line in input) {
+            val split = line.split(" | ")
+            val numbersToDisplay = split[1].split(" ")
+
+            val variants = numbersToDisplay
+                .groupingBy { it.length }
+                .eachCount().toMutableMap().toSortedMap()
+
+            val sum = variants
+                .filter { entry -> entry.key in 2..4 || entry.key == 7 }
+                .toMap()
+                .values.sum()
+            println("sum for lengthes 2, 3, 4, 7: $sum")
+
+            sumForTask += sum
+        }
+
+        return sumForTask
+    }
+}
